c982b28b9b6444a11cc4cf1ce1c2f8bf32d056ce
Vladimir Revyagin <skorabeyf@gmail.com>
2023-02-14 14:22:51 +0300

    Don't work

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Users from "./components/users";
 import SearchStatus from "./components/searchStatus";
-import api from "../api"
+import api from "./api"
 
 function App() {
     const [users, setUsers] = useState(api.users.fetchAll());
@@ -22,10 +22,7 @@ function App() {
     return (
         <div>
             <SearchStatus length={users.length} />
-            <Users 
-            onDelete={handleDelete} 
-            onToggleBookMark={handleToggleBookMark} 
-            users={users} />
+            <Users onDelete={handleDelete} onToggleBookMark={handleToggleBookMark} users={users} />
         </div>
     );
 }
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import User from "./user";
-
 const Users = ({ users, ...rest }) => {
     return (
         <>
@@ -10,7 +9,7 @@ const Users = ({ users, ...rest }) => {
                         <tr>
                             <th scope="col">Имя</th>
                             <th scope="col">Качества</th>
-                            <th scope="col">Профессия</th>
+                            <th scope="col">Провфессия</th>
                             <th scope="col">Встретился, раз</th>
                             <th scope="col">Оценка</th>
                             <th scope="col">Избранное</th>
