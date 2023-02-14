3656b5fd54b3c0588e72a5b03315b174b244cf8b
Vladimir Revyagin <skorabeyf@gmail.com>
2023-02-14 13:48:34 +0300

    Files for next task

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,58 +1,31 @@
-import React, {useState} from "react";
-import api from "../api";
+import React from "react";
+import User from "./user";
 
+const Users = ({ users, ...rest }) => {
+    return (
+        <>
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <User key={user._id} {...rest} {...user} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </>
+    );
+};
 
-const Users=()=>{
-  // Динамический рендер классов
-    const getBadgeClasses =()=>{
-    let colorClass = "badge "
-    colorClass+= users.length===0 ? "bg-warning text-dark" : "bg-primary"
-    return colorClass
-  }
-
-  // Работа с массивом
-  const [users, setUsers] = useState(api.users.fetchAll());
-  const handleDelete = (userId) =>{
-   setUsers((prevState)=>prevState.filter(user=>user._id!==userId
- ))
- }
-  //Изменеиние вида текста в зависимости от длины массива
- const handlePhrase = (number)=>{
-  number += users.length>=2 && users.length<=4 ? (` человека тусанут с тобой сегодня`) : (` человек тусанет с тобой сегодня`)
-   return number 
- }
- 
-  return (
-    <>
-  {users.length!==0
-    ? <><h1><span className={getBadgeClasses()}>{handlePhrase(users.length)}</span></h1>
-    <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">Имя</th>
-        <th scope="col">Качества</th>
-        <th scope="col">Профессия</th>
-        <th scope="col">Встретился, раз</th>
-        <th scope="col">Оценка</th>
-        <th scope="col"> </th>
-      </tr>
-    </thead>
-    <tbody>
-      {users.map((user)=>(<tr key={user._id}>
-        <td>{user.name}</td>
-        <td>{user.qualities.map(qualiti=>(<span key={qualiti._id} className={`badge bg-${qualiti.color}`}>{qualiti.name }</span>))}</td>
-        <td key={user.profession._id}>{user.profession.name}</td>
-        <td>{user.completedMeetings}</td>
-        <td>{user.rate}/5</td>
-        <td><button onClick={()=>handleDelete(user._id)}className="badge bg-danger">delete</button></td>
-        </tr>))}
-      </tbody>
-</table>
-    </>
-    
-    : <h1><span className={getBadgeClasses()}>Никто с тобой не тусанет</span></h1>
-  }
-    </>
-  )
-}
-export default Users
+export default Users;
